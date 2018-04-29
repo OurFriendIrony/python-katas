81e6b11d0765ae1d2c516cf350d9809e9140a93d
Steve Stevens <steve.h.i.stevens@gmail.com>
2018-04-29 18:33:48 +0100

    refactor lookup for 'unique' elements

--- src/RomanNumerals.py
+++ src/RomanNumerals.py
@@ -1,12 +1,11 @@
 class RomanNumerals:
+    ALL_ROMAN = ["IV", "V", "VI"]
+    ALL_ARABIC = [4, 5, 6]
+
     def convert(self, input):
         roman_string = ""
-        if input == 4:
-            roman_string = "IV"
-        elif input == 5:
-            roman_string = "V"
-        elif input == 6:
-            roman_string = "VI"
+        if self.ALL_ARABIC.count(input):
+            roman_string = self.ALL_ROMAN[self.ALL_ARABIC.index(input)]
         else:
             for x in range(0, input):
                 roman_string += "I"
